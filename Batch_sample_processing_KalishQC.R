d81acdb3f673264901236449456ea94250b2471c
Mike Dolan <mike.j.dolan2011@gmail.com>
2018-11-03 18:18:55 -0400

    Heavily modified code to aggregate accross timepoints

--- Batch_sample_processing_KalishQC.R
+++ Batch_sample_processing_KalishQC.R
@@ -1,4 +1,4 @@
-#Code to run through all the samples and do the same QC as the Kalish paper. 
+#Code to run through all the samples and do the same cell QC as the Kalish paper. 
 
 #Cells with fewer than 500 or more than 15,000 UMI counts were excluded. Note that this is not a great way to do QC 
 #accross different timepoints imo. The data were log-normalized and scaled to 10,000 transcripts per cell.
@@ -16,7 +16,6 @@ QC.plotter<-function(object) {
   median.mito<-median(data$percent.mito); print(paste0("Median mito is: ", median.mito))
   median.nUMI<-median(data$nUMI); print(paste0("Median nUMI is: ", median.nUMI))
   
-  
   g<-ggplot(data, aes(x=nUMI, y=percent.mito))
   g<-g+geom_point(size=0.5, alpha=0.05)
   g<-g+coord_cartesian(expand = F)
@@ -26,16 +25,50 @@ QC.plotter<-function(object) {
 }
 name.extract<-function(name) {
   name<-sapply(strsplit(name, "\\Q.\\E"), "[", 1)
-  name<-paste0(sapply(strsplit(name, "_"), "[", 1), "_", sapply(strsplit(name, "_"), "[", 2))
+  name<-paste0(sapply(strsplit(name, "_"), "[", 2), "_", sapply(strsplit(name, "_"), "[", 3))
   name
-  }
+}
+
 
 #Set count matrix data path (tsv files, not included in repo as very large)
 path<-"/Users/mdolan/Desktop/Kalish2018_Data/"
 setwd(path)
 samples<-list.files()
 
-#Loop through the samples and run the Kalish QC and save the RDS file and print 
+#List out the different timepoints
+time.points<-c("P5_", "P10_", "P16_", "P21_")
+
+#Loops through each timepoint, creates a Seurat object for first sample and iteratively merges the samples
+for(i in 1:4){ 
+  P.X<-time.points[i]
+  P.X.tsv<-grep(pattern = P.X, x = samples, value = TRUE, fixed = TRUE)
+  for(k in 1:length(P.X.tsv)) {
+    if(k==1) {
+      object<-read.table(file =P.X.tsv[k] , header = TRUE, row.names = 1, sep = '\t')
+      object<-t(object)
+      object<-CreateSeuratObject(raw.data = object , min.cells = 3, min.genes = 200, 
+                         project =P.X.tsv[k] )
+    } #Will initialize a Seurat object 
+    if(k>1) {
+      temp<-read.table(file =P.X.tsv[k] , header = TRUE, row.names = 1, sep = '\t')
+      temp<-t(temp)
+      temp<-CreateSeuratObject(raw.data = temp , min.cells = 3, min.genes = 200, 
+                                 project =P.X.tsv[k])
+      object<-MergeSeurat(object1 = object, object2 = temp, project = P.X, do.normalize = FALSE
+                          ,add.cell.id2 = P.X.tsv[k])
+      rm(temp)
+    } #Will merge subsequent Seurat samples
+  }
+  saveRDS(object = object, file = paste0(P.X, "_aggregated.rds"))
+  rm(object)
+}
+
+
+
+
+
+
+#Loop through the samples and run the Kalish QC and save the Seurat object as file. 
 for(i in 1:length(samples)) {
 
   #Load the data and initialize the object 
@@ -54,35 +87,38 @@ for(i in 1:length(samples)) {
   object<- FilterCells(object =object, subset.names = c("nUMI"), 
                          low.thresholds = c(500), high.thresholds = 15000)
   
+  #Save the object
   saveRDS(object, file = paste0(object_name, "_Kalishfiltered.rds"))
-
+  print(object_name)
 }
 
-  #Actually just want to do the below on the final dataset 
-  #Normalize the filtered dataset 
-  object<- NormalizeData(object = object, normalization.method = "LogNormalize", 
+
+#Actually just want to do the below on the final dataset 
+#Normalize the filtered dataset 
+P5<- NormalizeData(object = P5, normalization.method = "LogNormalize", 
                          scale.factor = 10000)
   
-  #Detect the most variable genes, play around with these to see what the higher average expression genes are? 
-  object<- FindVariableGenes(object =  object  , mean.function = ExpMean, dispersion.function = LogVMR, 
+#Detect the most variable genes, play around with these to see what the higher average expression genes are? 
+P5<- FindVariableGenes(object =  P5  , mean.function = ExpMean, dispersion.function = LogVMR, 
                                   x.low.cutoff = 0.0125, x.high.cutoff = 3, y.cutoff = 0.5)
   
-  #Scale the data accross the different genes and regress out variations due to nUMI (high expression) and per.cent mito
-  object <- ScaleData(object = object, vars.to.regress = c("nUMI", "percent.mito"))
+#Scale the data accross the different genes and regress out variations due to nUMI (high expression) and per.cent mito
+P5 <- ScaleData(object = P5, block.size = 100)
   
   
-  #Run the PCA. Keep PCs 1-30, as per Kalish 
-  object<- RunPCA(object = object, pc.genes = object@var.genes, do.print = TRUE, pcs.print = 1:5, 
+#Run the PCA. Keep PCs 1-30, as per Kalish 
+P5<- RunPCA(object = P5, pc.genes = P5@var.genes, do.print = TRUE, pcs.print = 1:5, 
                     genes.print = 5)
 
   
-  #what are significant PCs? 
-  
-  #Cluster the data 
-  object<- FindClusters(object = object, reduction.type = "pca", dims.use = 1:10, 
+#what are significant PCs? 
+PCElbowPlot(P5, num.pc = 1:30)  
+
+#Cluster the data 
+object<- FindClusters(object = object, reduction.type = "pca", dims.use = 1:10, 
                           resolution = 0.6, print.output = 0, save.SNN = FALSE)
   
-  #Use the clustering to inform a tSNE on the significant PCs
-  object<- RunTSNE(object = object, dims.use = 1:30, do.fast = TRUE)
-  TSNEPlot(object = object, do.label = TRUE, pt.size = 0.25)
+#Use the clustering to inform a tSNE on the significant PCs
+object<- RunTSNE(object = object, dims.use = 1:30, do.fast = TRUE)
+TSNEPlot(object = object, do.label = TRUE, pt.size = 0.25)
  
